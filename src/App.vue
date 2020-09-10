f1770fd1bc9a7d3078df00e9368371f8c01c5e93
abhishek <abhishekkumar0539@gmail.com>
2020-09-10 17:59:47 +0530

    complete

---
+++ src/App.vue
@@ -0,0 +1,34 @@
+<template>
+  <div id="app">
+    <HelloWorld msg="Vue App"/>
+    <input v-model="message" placeholder="Edit me">
+    <p>Message is: {{ message.toUpperCase() }}</p>
+  </div>
+</template>
+
+<script>
+import HelloWorld from './components/HelloWorld.vue'
+
+export default {
+  name: 'App',
+  components: {
+    HelloWorld,
+  },
+  data() {
+    return{
+    message: ""
+    }
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
